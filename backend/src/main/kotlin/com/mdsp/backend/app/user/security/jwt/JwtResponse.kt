730ad50df573f24f7845d7246bacad0f9a2b787f
suiebay <72402249+suiebay@users.noreply.github.com>
2021-03-28 20:23:10 +0500

    Delete backend directory

--- backend/src/main/kotlin/com/mdsp/backend/app/user/security/jwt/JwtResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.mdsp.backend.app.user.security.jwt
-
-import org.springframework.security.core.GrantedAuthority
-
-class JwtResponse(var accessToken: String?, var refreshToken: String?) {
-}
