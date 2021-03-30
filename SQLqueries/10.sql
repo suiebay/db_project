a35c45d051e6ed72fa835d617979e62adce8d1f2
suiebay <72402249+suiebay@users.noreply.github.com>
2021-03-30 15:47:10 +0500

    Update 10.sql

--- SQLqueries/10.sql
+++ SQLqueries/10.sql
@@ -1,4 +1,8 @@
-/*search for groups by title*/
+/*search for users, ordered by got points*/
 
-/*findAllByDeletedAtIsNullOrderByTitle*/
-SELECT * FROM reads_group WHERE deleted_at is null order by title
+/*findAllReadersPlace*/
+SELECT * FROM profiles AS u
+INNER JOIN users_roles ON u.id = users_roles.user_id
+INNER JOIN roles ON users_roles.role_id = roles.id
+WHERE roles.name = :role AND u.deleted_at IS NULL
+ORDER BY u.reads_point DESC, u.id
