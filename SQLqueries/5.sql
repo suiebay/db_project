ad27d00a29ebef60873b30d16f0b507bcf3b1583
suiebay <72402249+suiebay@users.noreply.github.com>
2021-03-30 15:51:23 +0500

    Update 5.sql

--- SQLqueries/5.sql
+++ SQLqueries/5.sql
@@ -1,4 +1,11 @@
-/*get all not deleted books*/
+/* search for users by specific text*/
 
-/* findAllByDeletedAtIsNull*/
-SELECT * FROM reads_books WHERE deleted_at is null
+/* findAllReadersBySearch*/
+SELECT * FROM profiles  AS u
+INNER JOIN users_roles ON u.id = users_roles.user_id
+INNER JOIN roles ON users_roles.role_id = roles.id
+WHERE roles.name = :role AND u.deleted_at IS NULL
+AND (LOWER (u.first_name) LIKE LOWER (:word)
+OR LOWER (u.last_name) LIKE LOWER(:word)
+OR LOWER (u.middle_name) LIKE LOWER(:word))
+ORDER BY u.reads_point DESC, u.id
